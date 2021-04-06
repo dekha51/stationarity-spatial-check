7f3cefe175ba99e1f2d6240d2a9d7f697ef354b3
Dekha <59503441+dekha51@users.noreply.github.com>
2021-04-06 14:03:16 +0700

    Add files via upload

---
+++ stationarity.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 15:34:54 2020
+
+@author: ASUS
+"""
+#stationarity checking using swath plot
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+df=pd.read_csv('chorizon.csv')
+df=df[['ID','XCOO','YCOO','Ag']]
+df=df[df.ID<800]
+
+# import geostatspy.geostats as geostats
+# #normalize value
+# df['Ag'],tvAg,tnsAg=geostats.nscore(df, 'Ag')
+
+plt.figure()
+plt.plot(df.ID,df.Ag,color='k')
+plt.title('Ag Field Data')
+plt.xlabel('ID')
+plt.ylabel('Ag')
+plt.show()
+
+from scipy import stats
+slope, intercept, corrcoeff, p_value, stderr  = stats.linregress(df.ID,df.Ag)
+
+m=slope
+c=intercept
+
+df.Y=m*df.ID+c
+
+plt.figure()
+plt.plot(df.ID,df.Ag,color='k') #point plotting
+plt.plot(df.ID,df.Y,linewidth=3.5,color='y',label='trend') #trend plotting
+plt.title('Ag Field Data')
+plt.xlabel('ID')
+plt.ylabel('Ag')
+plt.legend()
+plt.show()
+
+#sampling at certain ID
+df_part=df.iloc[503:573]
+
+slope, intercept, corrcoeff, p_value, stderr  = stats.linregress(df_part.ID,df_part.Ag)
+
+m=slope
+c=intercept
+
+df_part.Y=m*df_part.ID+c
+
+plt.figure()
+plt.plot(df_part.ID,df_part.Ag,color='k')
+plt.plot(df_part.ID,df_part.Y,linewidth=3.5,color='y',label='trend')
+plt.title('Ag Field Data')
+plt.xlabel('ID')
+plt.ylabel('Ag')
+plt.show()
+
+#outlier detection using IQR
+Q3 = df.Ag.describe().T['75%']
+Q1 = df.Ag.describe().T['25%']
+IQR = Q3 - Q1
+
+upper_fence = Q3 + (1.5 * IQR)
+lower_fence = Q1 - (1.5 * IQR)
+
+df_out=df[(df.Ag < lower_fence) | (df.Ag > upper_fence)]
+
+# plt.figure()
+# plt.plot(df_out.Ag)
+# plt.show()
+
---
+++ stationarity_vario.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  2 13:14:03 2020
+
+@author: ASUS
+"""
+#stationarity checking using variogram plot
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.close('all')
+
+df=pd.read_csv('chorizon.csv')
+df=df[['ID','XCOO','YCOO','Ag']]
+df=df[df.ID<800]
+
+import geostatspy.geostats as geostats
+# #normalize value
+# df['Ag'],tvAg,tnsAg=geostats.nscore(df, 'Ag')
+
+plt.figure()
+plt.plot(df.ID,df.Ag,color='k')
+plt.title('Ag Field Data')
+plt.xlabel('ID')
+plt.ylabel('Ag')
+plt.show()
+
+x=df.XCOO
+y=df.YCOO
+z=df.Ag
+
+#visualisasi
+import geostatspy.GSLIB as gslib
+plt.figure()
+cmap = plt.cm.plasma                                    # set the color map
+gslib.locmap_st(df,'XCOO','YCOO','Ag',min(x),max(x),min(y),7600000,min(z),max(z),'Ag Distribution','X (m)','','Ag Percentage (%)',cmap)
+
+
+
+#make variogram
+lag=7000
+lag_tol=3000
+nlag=30
+azi=0
+azi_tol=90
+bandwidth=9999
+lags, gammas, npps = geostats.gamv(df,"XCOO","YCOO","Ag",-9999,9999,lag,lag_tol,nlag,azi,azi_tol,bandwidth,isill=1.0)
+
+
+#variogram modelling
+nug = 0.5
+nst = 1 #whether a normal score transformation is used on this variogram
+it1 = 1; cc1 = 0.5; azi1 = azi; hmaj1 = 110000; hmin1 = 110000
+#it is the type of variogram (Spherical, exponential, gaussian)
+#assumption no contribution on minor structure
+it2 = 1; cc2 = 0; azi2 = 0; hmaj2 = 9999.9; hmin2 = 400  
+xlag=15000
+vario = gslib.make_variogram(nug,nst,it1,cc1,azi1,hmaj1,hmin1,it2,cc2,azi2,hmaj2,hmin2) # make model object
+
+index_maj,h_maj,gam_maj,cov_maj,ro_maj = geostats.vmodel(nlag,xlag,azi,vario) 
+
+# plot experimental variogram
+plt.figure()                                  
+plt.scatter(lags,gammas,color = 'black',s = npps*0.1,label = 'Azimuth ' +str(azi))
+plt.plot(h_maj,gam_maj,color = 'black')
+plt.legend()
+# plt.plot([0,175000],[1.0,1.0],color = 'black')
+# plt.plot([0,max(lags)],[1.0,1.0],color = 'black')
+plt.xlabel('Lag Distance')
+plt.ylabel('Variogram')
+plt.title('Ag Distribution Variogram')
+plt.xlim([0,175000]); plt.ylim([0,1.5])
+# plt.xlim([0,max(lags)]); plt.ylim([0,1.5])
+plt.grid()
+plt.show()
